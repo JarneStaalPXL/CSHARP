7d3400bd9e24c19dcf6337b60bd3770891b4fb62
JarneSt <12001144@student.pxl.be>
2020-10-30 18:36:05 +0100

    Updated

--- HomeProjects/WPF/UserRegistrator/UserRegistrator/MainWindow.xaml.cs
+++ HomeProjects/WPF/UserRegistrator/UserRegistrator/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace UserRegistrator
         {
             for (int i = 0; i < 1; i++)
             {
+                string[] names = new string[1];
+                string[] adress = new string[1];
                 names[i] = Console.ReadLine();
             }
         }
@@ -31,23 +33,49 @@ namespace UserRegistrator
         {
             for (int i = 0; i < 1; i++)
             {
+                string[] names = new string[1];
+                string[] adress = new string[1];
                 adress[i] = Console.ReadLine();
             }
         }
-
-        
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string filePath = Directory.GetCurrentDirectory(); //@"C:\Users\jarne\source\repos\Arraytest\";
             string dbPath = System.IO.Path.Combine(filePath, "Database");
+            string inputFile = System.IO.Path.Combine(dbPath, "SubmittedUserInfo.txt");
             System.IO.Directory.CreateDirectory(dbPath);
 
+        }
+
+        private void Output(object sender, RoutedEventArgs e)
+        {
+            string filePath = Directory.GetCurrentDirectory(); //@"C:\Users\jarne\source\repos\Arraytest\";
+            string dbPath = System.IO.Path.Combine(filePath, "Database");
             string inputFile = System.IO.Path.Combine(dbPath, "SubmittedUserInfo.txt");
 
+            const string UNDERLINE = "\x1B[4m";
+            const string RESET = "\x1B[0m";
+            Console.WriteLine(UNDERLINE + "\nSubmitted Data\n" + RESET);
 
-            Process myProcess = new Process();
-            Process.Start("notepad", @"C:\Users\12001144\OneDrive - PXL\PXL C#\HomeProjects\Console\Arraytest\Arraytest\bin\Debug\netcoreapp3.1\Database\SubmittedUserInfo.txt");
+            string[] names = new string[1];
+            string[] adress = new string[1];
+
+            StreamWriter writer = new StreamWriter(inputFile, true);
+            DateTime date1 = DateTime.Now;
+            writer.WriteLine($"------------------USER INFO------------------ \n{date1.ToString(System.Globalization.CultureInfo.InvariantCulture)} \n");
+            for (int j = 0; j < names.Length; j++)
+            {
+                writer.WriteLine($"Name: {names[j]}");
+                writer.WriteLine($"Adress: {adress[j]}\n");
+            }
+            writer.Close();
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine($"Name: {names[i]} \n");
+                Console.WriteLine($"Adress: {adress[i]} \n");
+            }
         }
     }
 }
