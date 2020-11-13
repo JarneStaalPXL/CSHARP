960e9448ad19aabe53c43ab46c8014db8c95c99e
JarneSt <12001144@student.pxl.be>
2020-11-13 14:22:14 +0100

    Added

---
+++ Week 5/Classes Oefeningen/Oefening 1/Oefening 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Oefening_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number 1 ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Enter number 2 ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Enter number 3 ");
+            int num3 = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+
+            int max = Math.Max(num3, Math.Max(num1, num2));
+
+            Console.WriteLine($"The max is " + max);
+        }
+    }
+}
