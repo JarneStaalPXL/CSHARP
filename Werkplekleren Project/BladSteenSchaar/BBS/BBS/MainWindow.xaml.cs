97892be94825d20cfa0ecbde724bdd3f2427f5f8
JarneSt <12001144@student.pxl.be>
2020-11-22 10:26:23 +0100

    Updated

--- Werkplekleren Project/BladSteenSchaar/BBS/BBS/MainWindow.xaml.cs
+++ Werkplekleren Project/BladSteenSchaar/BBS/BBS/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace BBS
 {
@@ -28,66 +29,72 @@ namespace BBS
         string PlayerChose;
         private int scoreplayer = 0;
         private int scorepc = 0;
-        private string msg = "   Result: ";
         private string computer = "Computer";
-        private string player = "Player";
-        private string draw = "Draw";
+        private string title = "Resultaat";
+        private string player = "Speler";
+        private string draw = "Gelijkspel";
 
         public MainWindow()
         {
+
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                this.timeLabel.Content = $"{DateTime.Now.ToString("d")}\n{DateTime.Now.ToString("HH:mm:ss")}";
+            },
+            this.Dispatcher);
         }
         public void BBS() // The Rock Paper Scissors-game
         {
-            scoreplayer1.Content = ($"PLAYER {scoreplayer}"); //old score
-            scorepc1.Content = ($"PC: {scorepc}"); //old score
+            scoreplayer1.Content = ($"SPELER {scoreplayer}"); //old score
+            scorepc1.Content = ($"COMPUTER {scorepc}"); //old score
 
             if (PlayerChose == "Rock" && Computer == "Paper") // Player: Rock, Computer: paper = computer wins
             {
-                MessageBox.Show(msg + computer);
+                MessageBox.Show($"   {computer, title}");
                 scorepc++;
             }
             else if (PlayerChose == "Rock" && Computer == "Scissors") // Player: Rock, Computer: Scissors = Player wins
             {
-                MessageBox.Show(msg + player);
+                MessageBox.Show(player, title);
                 scoreplayer++;
             }
             else if (PlayerChose == "Paper" && Computer == "Scissors") // Player: Paper, Computer: Scissors = Computer wins
             {
-                MessageBox.Show(msg + computer);
+                MessageBox.Show(computer, title);
                 scorepc++;
             }
             else if (PlayerChose == "Paper" && Computer == "Rock") // Player: Paper, Computer: Rock = Player wins
             {
-                MessageBox.Show(msg + player);
+                MessageBox.Show(player, title);
                 scoreplayer++;
             }
             else if (PlayerChose == "Scissors" && Computer == "Rock") // Player: Scissors, Computer: Rock = Computer wins
             {
-                MessageBox.Show(msg + computer);
+                MessageBox.Show(computer, title);
                 scorepc++;
             }
             else if (PlayerChose == "Scissors" && Computer == "Paper") // Player: Scissors, Computer: Paper = Player wins
             {
-                MessageBox.Show(msg + player);
+                MessageBox.Show(player, title);
                 scoreplayer++;
             }
             else if (PlayerChose == "Scissors" && Computer == "Scissor")
             {
-                MessageBox.Show(msg + draw);
+                MessageBox.Show(draw);
 
             }
             else if (PlayerChose == "Paper" && Computer == "Paper")
             {
-                MessageBox.Show(msg + draw);
+                MessageBox.Show(draw);
             }
             else if (PlayerChose == "Rock" && Computer == "Rock")
             {
-                MessageBox.Show(msg + draw);
+                MessageBox.Show(draw);
             }
 
-            scoreplayer1.Content = ($"PLAYER {scoreplayer}"); //new score
-            scorepc1.Content = ($"PC: {scorepc}"); //new score 
+            scoreplayer1.Content = ($"SPELER {scoreplayer}"); //new score
+            scorepc1.Content = ($"COMPUTER {scorepc}"); //new score 
 
         }
         private void RockClick(object sender, RoutedEventArgs e)
