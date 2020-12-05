0496d431db7bd18ef189c6ceed60e5d7054c108a
JarneSt <12001144@student.pxl.be>
2020-12-05 11:32:15 +0100

    Updated

---
+++ Week 10/Arrays/Oefening 1/Oefening 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Oefening_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vijfstudenten = new string[5];
+            for (int i = 0; i < vijfstudenten.Length; i++)
+            {
+                Console.Write("Input names: ");
+                vijfstudenten[i] = Console.ReadLine();
+                Console.Clear();
+            }
+            //for (int i = 0; i < vijftienstudenten.Length; i++)
+            //{
+            //    Console.Write($"{vijftienstudenten[i]}  ");
+            //}
+            Console.WriteLine("Enter number");
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine(vijfstudenten[input]); 
+        }
+    }
+}
---
+++ Week 10/Arrays/Oefening 2/Oefening 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Oefening_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) //gevaarlijk XD
+            {
+                Console.WriteLine("\nEnter size of array");
+                IsLangerDanTien();
+            }
+
+        }
+
+        private static void IsLangerDanTien()
+        {
+            int input = int.Parse(Console.ReadLine());
+            int[] TienGetallen = new int[input];
+
+            if (TienGetallen.Length > 10)
+            {
+                Console.WriteLine($"De Array is groter dan 10. Hij bevat {input} waardes");
+
+            }
+            else
+            {
+                Console.WriteLine($"De Array is kleiner dan 10. Hij bevat {input} waardes");
+            }
+        }
+    }
+}
