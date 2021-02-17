bdce21316a514510daa69a030a5a6255343f84fb
Jarne Staal <68494791+JarneSt@users.noreply.github.com>
2021-02-17 10:14:14 +0100

    Changed/Added

--- Semester 2/Oefening 1 EmailBestand/Oefening 1 EmailBestand/MainWindow.xaml.cs
+++ Semester 2/Oefening 1 EmailBestand/Oefening 1 EmailBestand/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,50 @@ namespace Oefening_1_EmailBestand
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Dictionary<string, string> dicGeg = new Dictionary<string, string>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Inlezen(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            string pad = @"..\..\Bestanden\Email.txt";
+            sb.Append(File.ReadAllText(pad));
+
+            txtResultaat.Text = sb.ToString();
+        }
+
+        private void dialogInlezen(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            txtResultaat.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void dictionRead(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            string pad = File.ReadAllText(openFileDialog.FileName);
+
+            using (var sr = new StreamReader(pad))
+            {
+                string line = null;
+
+                // while it reads a key
+                while ((line = sr.ReadLine()) != null)
+                {
+                    // add the key and whatever it 
+                    // can read next as the value
+                    dicGeg.Add(line, sr.ReadLine());
+                }
+            }
+
+
+            txtResultaat.Text = dicGeg.ToString() ;
+            
         }
     }
 }
